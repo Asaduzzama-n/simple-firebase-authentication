c07fbc07dba5588459fa6d3825f8422ea0a430b2
Asaduzzaman <masaduzzaman193146@bscse.uiu.ac.bd>
2023-02-19 08:35:51 +0600

    after github authentication

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import app from './firebase/firebase.init'
 import { useState } from 'react';
 
@@ -10,6 +10,19 @@ function App() {
 
   const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
+  const gitProvider = new GithubAuthProvider();
+
+  const handleGitSignIn = () =>{
+    signInWithPopup(auth,gitProvider)
+    .then(result => {
+      const user = result.user;
+      setUser(user);
+      console.log(user);
+    })
+    .catch(error =>{
+      console.error(error);
+    })
+  }
 
 
   const handleGoogleSignIn = () => {
@@ -38,10 +51,16 @@ function App() {
   return (
     <div className="App">
       {
-        user.email ? <button onClick={handleSignOut}>Sign Out</button> : <button onClick={handleGoogleSignIn}>Google Sign In</button>
+        user.uid ? <button onClick={handleSignOut}>Sign Out</button> : 
+        <>
+        <button onClick={handleGoogleSignIn}>Google Sign In</button>
+        <button onClick={handleGitSignIn}>GitHub Sign In</button>
+
+        </>
       }
+      
       {
-        user.email && <div className="user-info">
+        user.uid && <div className="user-info">
           <h1>User Name: {user.displayName}</h1>
           <h4>Email: {user.email}</h4>
           <img src={user.photoURL} alt="" />
